3587d94cf0509065f27f27beee5a12dc1c156652
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-26 11:30:48 -0400

    add intern class

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,4 +1,17 @@
+const Intern = require('../lib/Intern');
 
-test('placeholder', () => {
-    expect('10').toBe('10');
+test('create intern object', () => {
+    const intern = new Intern('Billy Joel', 3, 'pianoman@gmail.com', 'New York City');
+
+    expect(intern.name).toBe('Billy Joel');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toBe('pianoman@gmail.com');
+    expect(intern.school).toBe('New York City');
 });
+
+test('intern methods', () => {
+    const intern = new Intern('Phil', 1, 'phil@phil.com', 'Philly');
+
+    expect(intern.getRole()).toBe('Intern');
+    expect(intern.getSchool()).toBe('Philly');
+})
