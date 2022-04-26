11e558ec2d8981f9f8b8764f0a612cc76c57c482
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-26 09:21:52 -0400

    added employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,18 @@
+const Employee = require('../lib/Employee');
+
+test('create an employee class', () => {
+    const employee = new Employee('John', 0,'good@email.com');
+
+    expect(employee.name).toBe('John');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('good@email.com');
+});
+
+test('test employee methods', () => {
+    const employee = new Employee('Martin', 2, 'ope@geedang.com');
+
+    expect(employee.getName()).toBe('Martin');
+    expect(employee.getId()).toEqual(expect.any(Number));
+    expect(employee.getEmail()).toBe('ope@geedang.com');
+    expect(employee.getRole()).toBe('Employee');
+});
