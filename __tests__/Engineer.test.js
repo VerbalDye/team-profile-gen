0414a4882433c8f05bae08c0a10b68ffef0c9d15
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-26 10:08:47 -0400

    added engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,17 @@
+const Engineer = require('../lib/Engineer');
 
-test('placeholder', () => {
-    expect('10').toBe('10');
+test('create the engineer object', () => {
+    const engineer = new Engineer('Garfield', 3, 'lasagna@orange.cat', 'OdieDroolz');
+
+    expect(engineer.name).toBe('Garfield');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toBe('lasagna@orange.cat');
+    expect(engineer.github).toBe('OdieDroolz');
 });
+
+test('engineer methods test', () => {
+    const engineer = new Engineer('Pearl', 2, 'daddiesgirl@mammal.sea', 'BreathTheAir3');
+
+    expect(engineer.getRole()).toBe('Engineer');
+    expect(engineer.getGithub()).toBe('BreathTheAir3');
+})
